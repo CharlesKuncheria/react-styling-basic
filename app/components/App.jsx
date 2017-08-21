d02b5e2b0405a6a169108930be8768bcf98b4ad8
CharlesKuncheria <charles.kuncheria@gmail.com>
2017-08-21 12:14:53 +0530

    Initial Commit

---
+++ app/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/styles.css'
+
+
+let App = (props) => (
+  <div className='flex-container'>
+    <div className='flex-item'>First</div>
+    <div className='flex-item'>Second</div>
+  </div>
+)
+
+export default App;
